904044cbc970b1cc04af82e2bce395d8e6c29060
Nathan Uphoff <Nathan@MacBook-Pro-14.local>
2024-05-11 16:01:13 +0200

    Add basic graphql client factory

---
+++ src/utils/graphql/createReadQuery.ts
@@ -0,0 +1,27 @@
+/**
+ * Simple wrapper to fetch graphql data with optimistic types.
+ * @param endpoint {string} The API endpoint url.
+ * @returns {object} The requested data.
+ * @todo replace with proper graph-ql library.
+ */
+export function createReadQuery(endpoint: string) {
+  return async function query<Data>(query: string): Promise<Data> {
+    const headers = new Headers([
+      ['Accept', 'application/json'],
+      ['Content-Type', 'application/json'],
+      ['X-Method-Used', 'graphiql'],
+    ]);
+
+    const response = await fetch(endpoint, {
+      method: 'POST',
+      headers,
+      body: JSON.stringify({
+        query,
+      }),
+    });
+
+    const responseData = await response.json();
+
+    return responseData.data;
+  };
+}
